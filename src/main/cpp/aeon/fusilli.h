944f347eaa144696dd7d8dc17d0e098ca4723d24
Kenneth Leung <ccllnekk@gmail.com>
2016-04-30 17:12:11 -0700

    license info update

--- src/main/cpp/aeon/fusilli.h
+++ src/main/cpp/aeon/fusilli.h
@@ -1,5 +1,4 @@
-/*
- * Licensed under the Apache License, Version 2.0 (the "License");
+/* Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
  * You may obtain a copy of the License at
  *
@@ -11,8 +10,7 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  *
- * Copyright (c) Kenneth Leung. All rights reserved.
- */
+ * Copyright (c) 2013-2016, Kenneth Leung. All rights reserved. */
 
 #pragma once
 //////////////////////////////////////////////////////////////////////////////
